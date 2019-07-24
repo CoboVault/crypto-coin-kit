4337a19a0a063d585c9fd0f87be8bec8dc05675c
Tian Li <tli2@cobo.com>
2019-07-24 18:22:28 +0800

    modify build script

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,10 @@
 import { KeyProvider } from "./keyProvider";
+import NEO from "./NEO";
 import { XRP } from "./XRP";
 declare const _default: {
     coins: {
         XRP: typeof XRP;
+        NEO: typeof NEO;
     };
     KeyProvider: typeof KeyProvider;
 };
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var keyProvider_1 = require("./keyProvider");
+var NEO_1 = __importDefault(require("./NEO"));
 var XRP_1 = require("./XRP");
 exports.default = {
     coins: {
-        XRP: XRP_1.XRP
+        XRP: XRP_1.XRP,
+        NEO: NEO_1.default
     },
     KeyProvider: keyProvider_1.KeyProvider
 };
