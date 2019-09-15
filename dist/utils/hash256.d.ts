742f7952a74fdfe7ce263c04c56e5c99ba265625
Tian Li <tli2@cobo.com>
2019-09-15 15:59:19 +0800

    run build

--- dist/utils/hash256.d.ts
+++ dist/utils/hash256.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="node" />
+export declare const sha256: (data: Buffer) => Buffer;
+export declare const blake256: (data: Buffer) => any;
 declare const _default: (data: Buffer) => Buffer;
 export default _default;
+export declare const doubleBlake256: (data: Buffer) => any;
