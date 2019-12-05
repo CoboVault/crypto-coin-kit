66e2a843aed7efaaffbbf962e7f4534de1abc8b9
zyang <zyang@cobo.com>
2019-12-05 15:45:46 +0800

    webpack build: multi entry, umd output

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/**
+ * @file webpack build for j2v8 call this lib, Multi entry for each coin and utils
+ */
+const path = require('path');
+
+module.exports = {
+  entry: {
+    BTC: './src/BTC/index.ts',
+    DCR: './src/DCR/index.ts',
+    ETC: './src/ETC/index.ts',
+    ETH: './src/ETH/index.ts',
+    NEO: './src/NEO/index.ts',
+    XRP: './src/XRP/index.ts',
+    XZC: './src/XZC/index.ts',
+    utils: './src/utils/index.ts',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist/subBundle')
+  }
+};
