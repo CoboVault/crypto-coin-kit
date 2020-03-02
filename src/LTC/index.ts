7f18f932e10dcb75aa29a536bec2e4497ea76b6b
ZhangJun <jzhang3@cobo.com>
2020-03-02 14:12:35 +0800

    support LTC legacy address tx

--- src/LTC/index.ts
+++ src/LTC/index.ts
@@ -1,14 +1,28 @@
 import * as bitcoin from 'bitcoinjs-lib'
 // @ts-ignore
 import bs58check from "bs58check";
-import {BTC} from "../BTC";
+import {BTC, Destination, TxData, TxOutputItem} from "../BTC";
 import {bitcoin as bitcoinNetwork, litecoin} from "../BTC_FORK/networks";
+import {KeyProvider, KeyProviderSync} from "../Common/sign";
 
 export class LTC extends BTC {
     constructor() {
         super();
         this.network = litecoin;
     }
+
+    public generateTransaction = async (
+        txData: TxData,
+        signers: KeyProvider[]) => {
+        return super.generateTransaction(this.processTxData(txData),signers);
+    };
+
+    public generateTransactionSync = (
+        txData: TxData,
+        signers: KeyProviderSync[]) => {
+        return super.generateTransactionSync(this.processTxData(txData),signers);
+    };
+
 
     public isAddressValid(address: string): boolean {
      try {
@@ -33,5 +47,37 @@ export class LTC extends BTC {
             default:
                 return address;
         }
+    }
+
+
+
+    private isDestinationOutputs = (
+        out: TxOutputItem[] | Destination
+    ): out is Destination => {
+        const output = out as Destination;
+        return (
+            output.to !== undefined &&
+            output.amount !== undefined &&
+            output.fee !== undefined &&
+            output.changeAddress !== undefined
+        );
+    };
+
+    private convertLegacyAddress(legacyAddress: string): string {
+        if (this.isAddressValid(legacyAddress) && legacyAddress.startsWith("3")) {
+            return this.convertAddress(legacyAddress);
+        }
+        return legacyAddress;
+    }
+
+    private processTxData = (txData:TxData):TxData => {
+        const processedTxData = txData;
+        if (this.isDestinationOutputs(processedTxData.outputs)) {
+            processedTxData.outputs.to = this.convertLegacyAddress(processedTxData.outputs.to);
+            processedTxData.outputs.changeAddress = this.convertLegacyAddress(processedTxData.outputs.changeAddress);
+        } else {
+            processedTxData.outputs.forEach(out => out.address =this.convertLegacyAddress(out.address))
+        }
+        return processedTxData;
     }
 }
