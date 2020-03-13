8c9c35b0c62aee6b2451932c1eef650f1db313b0
Sora <guess@who.iam>
2020-03-13 21:20:15 +0800

    fix eos token bug

--- src/__tests__/coins/EOS.test.ts
+++ src/__tests__/coins/EOS.test.ts
@@ -10,14 +10,17 @@ const txData:TxData = {
         to:"cobotestbibi",
         amount:10000,
         memo:'',
-        fee:0
+        fee:0,
+        symbol: 'NUT',
+        decimal: 9,
     },
     header:{
-        time: 1580807523825,
-        refBlockNum:103404960,
-        refBlockPrefix:508621698,
+        time: 1584104759699,
+        refBlockNum:109920658,
+        refBlockPrefix:192835320,
         expireInSeconds:300
-    }
+    },
+    tokenAccount: 'eosdtnutoken'
 
 };
 describe("Coin.EOS",()=>{
@@ -36,19 +39,19 @@ describe("Coin.EOS",()=>{
     // https://bloks.io/transaction/7af10fb78976604e080b0fe34c523e5ec722b800f4a9b1af350d6238039560dd
     it('should generate tx',async () => {
         const eosTx = await eos.generateTransaction(txData,SignProviderWithPrivateKey(privKey));
-        expect(eosTx.txId).toBe('7af10fb78976604e080b0fe34c523e5ec722b800f4a9b1af350d6238039560dd');
+        expect(eosTx.txId).toBe('350849c86f988659a83a0509d02a37a729feb10eb1f16c3e84f25d691f4a49b9');
         const tx = JSON.parse(eosTx.txHex);
         expect(tx.signatures[0])
-            .toBe('SIG_K1_Kf8yNx2UXUgqxuCMWzaune51QEE7BPungQiVcKx8dKtfhVPkc5SCjB4fPrUbqi2avEzioG6da3NR19j77dG3wmGaX3ssNS')
+            .toBe('SIG_K1_JvuGPLCqxJWjoULXVVftEZJ8BT5VWkjh4o1RgpeFegRMiMG25akDHn8TNoPZ6VPTzwSz3iEeJ7k4QaCLBK6ERE9oJAHprR')
     });
 
     //  curl -X POST --url https://api.eosnewyork.io/v1/chain/push_transaction -d '{"compression":"none","transaction":{"expiration":"2020-02-04T09:17:03","ref_block_num":54688,"ref_block_prefix":508621698,"max_net_usage_words":0,"max_cpu_usage_ms":0,"delay_sec":0,"context_free_actions":[],"actions":[{"account":"eosio.token","name":"transfer","authorization":[{"actor":"cobowalletcn","permission":"active"}],"data":"305156311a4e0f45e08e3b19ab4c0f45102700000000000004454f530000000000"}],"transaction_extensions":[]},"signatures":["SIG_K1_JxRVz8WGEeaJtdy27UmqT68E2pVB62u1hBBL98xL2Cn4kmpQ7DeGNkjtzhzCFmt7Tn7SYfRJnnCRcyny4rLCGkenAZgPDD"]}'
     // https://bloks.io/transaction/7af10fb78976604e080b0fe34c523e5ec722b800f4a9b1af350d6238039560dd
     it('should generate tx sync',   () => {
         const eosTx = eos.generateTransactionSync(txData,SignProviderWithPrivateKeySync(privKey));
-        expect(eosTx.txId).toBe('7af10fb78976604e080b0fe34c523e5ec722b800f4a9b1af350d6238039560dd');
+        expect(eosTx.txId).toBe('350849c86f988659a83a0509d02a37a729feb10eb1f16c3e84f25d691f4a49b9');
         const tx = JSON.parse(eosTx.txHex);
         expect(tx.signatures[0])
-            .toBe('SIG_K1_Kf8yNx2UXUgqxuCMWzaune51QEE7BPungQiVcKx8dKtfhVPkc5SCjB4fPrUbqi2avEzioG6da3NR19j77dG3wmGaX3ssNS')
+            .toBe('SIG_K1_JvuGPLCqxJWjoULXVVftEZJ8BT5VWkjh4o1RgpeFegRMiMG25akDHn8TNoPZ6VPTzwSz3iEeJ7k4QaCLBK6ERE9oJAHprR')
     });
 });
